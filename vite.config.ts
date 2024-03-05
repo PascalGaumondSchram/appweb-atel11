963be53093830ee0ebd7770559a597a5c1cac6b2
Pascal GS <118366389+PascalGaumondSchram@users.noreply.github.com>
2024-03-05 16:26:12 -0500

    Ajout workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+base: "/appweb-atel11/"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],

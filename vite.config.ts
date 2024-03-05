8b57dbabad799567a29268d51fded26303882ae7
Pascal GS <118366389+PascalGaumondSchram@users.noreply.github.com>
2024-03-05 16:38:36 -0500

    allo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-base: "/appweb-atel11/"
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/appweb-atel11/",
   plugins: [vue()],
 })
